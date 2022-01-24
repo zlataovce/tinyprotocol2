e5105fff7a8252d56476d59a2d5099122d0a96e3
zlataovce <34477304+zlataovce@users.noreply.github.com>
2022-01-24 21:02:13 +0100

    more friendly options

--- src/main/kotlin/me/kcra/hydrazine/HydrazinePluginExtension.kt
+++ src/main/kotlin/me/kcra/hydrazine/HydrazinePluginExtension.kt
@@ -7,9 +7,10 @@ abstract class HydrazinePluginExtension {
     var packageName: String? = null
     var mutable = false
 
-    fun packet(def: String) = packets.add(def)
+    fun packet(vararg def: String) = packets.addAll(def)
 
     fun version(ver: String) = versions.put(ver, -1)
+    fun version(vararg ver: String) = ver.forEach { versions[it] = -1 }
     fun version(ver: String, protocol: Int) = if (protocol > -1) versions.put(ver, protocol)
         else throw IllegalArgumentException("Protocol version must be zero or higher")
 }
