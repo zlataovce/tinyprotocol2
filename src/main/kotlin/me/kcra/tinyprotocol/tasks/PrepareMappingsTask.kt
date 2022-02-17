07c71dd26bc69365fa3e0baca2262619b79fc98a
zlataovce <34477304+zlataovce@users.noreply.github.com>
2022-02-17 15:02:10 +0100

    multi-threading for PrepareMappingsTask (wip)

--- src/main/kotlin/me/kcra/tinyprotocol/tasks/PrepareMappingsTask.kt
+++ src/main/kotlin/me/kcra/tinyprotocol/tasks/PrepareMappingsTask.kt
@@ -11,6 +11,7 @@ import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.nio.file.Path
+import java.util.concurrent.CompletableFuture
 import javax.inject.Inject
 
 abstract class PrepareMappingsTask @Inject constructor(private val extension: TinyProtocolPluginExtension) : DefaultTask() {
@@ -22,21 +23,24 @@ abstract class PrepareMappingsTask @Inject constructor(private val extension: Ti
     @TaskAction
     fun run() {
         val workFolder: File = Path.of(project.buildDir.absolutePath, "tinyprotocol").toFile().also { it.mkdirs() }
-        val files: MutableList<TypedMappingFile> = mutableListOf()
+        val futures: MutableList<CompletableFuture<TypedMappingFile>> = mutableListOf()
+
         for (ver: String in extension.versions.keys) {
-            files.add(
-                SrgUtilsMappingLoader.of(
-                    // order matters
-                    Pair.of(MappingType.MOJANG, minecraftResource(ver, "server_mappings", workFolder, extension.verifyChecksums)?.let { IMappingFile.load(it).reverse() }),
-                    Pair.of(MappingType.INTERMEDIARY, intermediaryMapping(ver, workFolder, extension.verifyChecksums)),
-                    Pair.of(MappingType.SEARGE, seargeMapping(ver, workFolder, extension.verifyChecksums)?.let { IMappingFile.load(it) }),
-                    Pair.of(MappingType.SPIGOT, spigotMapping(ver, workFolder, extension.verifyChecksums))
-                ).loadTyped()
+            futures.add(
+                CompletableFuture.supplyAsync {
+                    return@supplyAsync SrgUtilsMappingLoader.of(
+                        // order matters
+                        Pair.of(MappingType.MOJANG, minecraftResource(ver, "server_mappings", workFolder, extension.verifyChecksums)?.let { IMappingFile.load(it).reverse() }),
+                        Pair.of(MappingType.INTERMEDIARY, intermediaryMapping(ver, workFolder, extension.verifyChecksums)),
+                        Pair.of(MappingType.SEARGE, seargeMapping(ver, workFolder, extension.verifyChecksums)?.let { IMappingFile.load(it) }),
+                        Pair.of(MappingType.SPIGOT, spigotMapping(ver, workFolder, extension.verifyChecksums))
+                    ).loadTyped().also { logger.log(LogLevel.LIFECYCLE, "Loaded mappings for $ver.") }
+                }
             )
-            logger.log(LogLevel.LIFECYCLE, "Loaded mappings for $ver.")
         }
+
         project.tasks.withType(GeneratePacketsTask::class.java) {
-            it.mappings = files
+            it.mappings = futures.stream().map { f -> f.join() }.toList()
         }
     }
 }
